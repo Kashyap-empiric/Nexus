9b129b226debd70b6d3d24addc420280a8c3e592
Kashyap <Kashyap.k@empiricinfotech.com>
2026-06-09 15:21:36 +0530

    Clean seperation of useGlobalSocket

---
+++ client/src/modules/chat/realtime/index.ts
@@ -0,0 +1,10 @@
+import type { QueryClient } from "@tanstack/react-query";
+import { handleMessageNew } from "./message.handlers";
+import { handleMessageRead } from "./conversation.handlers";
+
+export const createChatEventRouter = (queryClient: QueryClient) => {
+  return {
+    messageNew: handleMessageNew(queryClient),
+    messageRead: handleMessageRead(queryClient),
+  };
+};
